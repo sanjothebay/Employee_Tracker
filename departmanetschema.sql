fe0679df441ab708a4fd628e7ec54dee9c15d132
JC <sanjothebay@gmail.com>
2020-12-18 19:09:30 -0600

    first table

---
+++ departmanetschema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS department_db;
+
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL
+);
+
+
+
+SELECT * FROM songs
