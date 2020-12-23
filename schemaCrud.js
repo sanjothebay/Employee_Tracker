120eb5cde733184dc30014a25492643cc133a6e8
JC <sanjothebay@gmail.com>
2020-12-22 20:26:06 -0600

    fixing one fuction

--- schemaCrud.js
+++ schemaCrud.js
@@ -85,86 +85,131 @@ function ViewAllEpmloyeeFunction() {
 }
 
 function AddEpmloyeeFunction() {
-  connection.query(
-    "INSERT INTO employee (id, first_name, last_name, role_id, manager_id)",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-      //employeeTrackerStartQuestionsFunction();
-    }
-  );
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the New Employee`s ID Number?",
+        name: "NewEmployeeIdInput",
+      },
+      {
+        type: "input",
+        message: "What is the New Employee`s First Name?",
+        name: "NewEmployeeFirstName",
+      },
+      {
+        type: "input",
+        message: "What is the New Employee`s Last Name?",
+        name: "NewEmployeeLastName",
+      },
+      {
+        type: "list",
+        message: "What is the New Employee`s Role Id?",
+        name: "NewEmployeeRoleId",
+        choices: ["88", "14", "5", "10"],
+      },
+      {
+        type: "list",
+        message: "What is the Manager Id`s of the New Employee?",
+        name: "NewEmployeeManagerId",
+        choices: ["1", "14"],
+      },
+    ])
+    .then(
+      function (Data) {
+        console.log(Data);
+        "INSERT INTO employee SET ?",
+          {
+            id: Data.NewEmployeeIdInput,
+            first_name: Data.NewEmployeeFirstName,
+            last_name: Data.NewEmployeeLastName,
+            role_id: Data.NewEmployeeRoleId,
+            manager_id: Data.NewEmployeeManagerId,
+          };
+      },
+      function (error) {
+        if (error) throw error;
+        console.log("Added New Employee To Database");
+      }
+    );
 }
 
-function RemoveEpployeeFunction() {
-  connection.query(
-    "DELETE FROM employee id, first_name, last_name, role_id, manager_id",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-      //employeeTrackerStartQuestionsFunction();
-    }
-  );
-}
+// function AddEpmloyeeFunction() {
+//   connection.query(
+//     "INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES ?",
+//     ("id", "first_name", "last_name", "role_id", "manager_id"),
+//     function (err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       connection.end();
+//       //employeeTrackerStartQuestionsFunction();
+//     }
+//   );
+// }
 
-function UpdateEmployeeRoleFunction() {
-  connection.query(
-    "SELECT employee.id, first_name, last_name, title FROM employee INNER JOIN roles ON employee.id = roles.id",
-    "UPDATE SET title =   ; WHERE roles.id =  ;",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-      //employeeTrackerStartQuestionsFunction();
-    }
-  );
-}
+// function RemoveEpployeeFunction() {
+//   connection.query(
+//     "DELETE FROM employee id, first_name, last_name, role_id, manager_id",
+//     function (err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       connection.end();
+//       //employeeTrackerStartQuestionsFunction();
+//     }
+//   );
+// }
 
-function UpdateEmplpoyeeManagerFunction() {
-  connection.query(
-    "SELECT id, first_name, last_name FROM employee, manager_id",
-    "UPDATE SET manager_id =   ; WHERE employee.manager_id =  ;",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-      //employeeTrackerStartQuestionsFunction();
-    }
-  );
-}
+// function UpdateEmployeeRoleFunction() {
+//   connection.query(
+//     "SELECT employee.id, first_name, last_name, title FROM employee INNER JOIN roles ON employee.id = roles.id",
+//     "UPDATE SET title =   ; WHERE roles.id =  ;",
+//     function (err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       connection.end();
+//       //employeeTrackerStartQuestionsFunction();
+//     }
+//   );
+// }
 
-function ViewAllRolesFunction() {
-  connection.query(
-    "SELECT employee.id, first_name, last_name, title FROM employee INNER JOIN roles ON employee.id = roles.id",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-    }
-  );
-}
+// function UpdateEmplpoyeeManagerFunction() {
+//   connection.query(
+//     "SELECT id, first_name, last_name FROM employee, manager_id",
+//     "UPDATE SET manager_id =   ; WHERE employee.manager_id =  ;",
+//     function (err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       connection.end();
+//       //employeeTrackerStartQuestionsFunction();
+//     }
+//   );
+// }
 
-function AddRoleFunction() {
-  connection.query(
-    "INSERT INTO roles (role_id,)",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-      //employeeTrackerStartQuestionsFunction();
-    }
-  );
-}
+// function ViewAllRolesFunction() {
+//   connection.query(
+//     "SELECT employee.id, first_name, last_name, title FROM employee INNER JOIN roles ON employee.id = roles.id",
+//     function (err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       connection.end();
+//     }
+//   );
+// }
 
-function RemoveRoleFunction() {
-  connection.query(
-    "DELETE FROM role role_id,",
-    function (err, res) {
-      if (err) throw err;
-      console.table(res);
-      connection.end();
-      //employeeTrackerStartQuestionsFunction();
-    }
-  );
-}
+// function AddRoleFunction() {
+//   connection.query("INSERT INTO roles (role_id,)", function (err, res) {
+//     if (err) throw err;
+//     console.table(res);
+//     connection.end();
+//     //employeeTrackerStartQuestionsFunction();
+//   });
+// }
+
+// function RemoveRoleFunction() {
+//   connection.query("DELETE FROM role role_id,", function (err, res) {
+//     if (err) throw err;
+//     console.table(res);
+//     connection.end();
+//     //employeeTrackerStartQuestionsFunction();
+//   });
+// }
--- schemaSeed.sql
+++ schemaSeed.sql
@@ -25,16 +25,16 @@ CREATE TABLE department (
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (14, "JC", "Rodriguez", 14, 1), (4, "James", "Rios", 24, 2), 
-(2, "Helena", "Rodriguez", 7, 1), (1, "Kryptonite", "Cruz", 88, null), 
+(2, "Helena", "Rodriguez", 5, 1), (1, "Kryptonite", "Cruz", 88, null), 
 (5, "John", "Carter", 10, 1), (7, "Janice", "Jones", 5, 2);
 
 INSERT INTO roles (id, title, salary, department_id)
-VALUE (88, "Manager", 50.000, 1),(14, "Engineer", 35.000, 2),
-(24, "Engineer", 35.000, 2), (7, "Developer", 25.000, 03), 
+VALUE (88, "Manager", 50.000, 1),(14, "Manager", 50.000, 2),
+(24, "Engineer", 35.000, 2),
 (5, "Developer", 25.000, 03), (10, "Intern", 10.000, 04);
 
 INSERT INTO department (id,name)
-VALUE (1, "Managers_Dept"), (14, "Engineering_Dept"),
+VALUE (1, "Managers_Dept"), (14, "Managers_Dept"),
 (4, "Engineering_Dept"), (2, "Developing_Dept"), 
 (7, "Developing_Dept"), (5, "Intern_Dept");
 
